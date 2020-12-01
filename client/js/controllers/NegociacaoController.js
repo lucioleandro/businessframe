349669487f62ffb50ee7f44a44045d1b382daa6e
Lúcio Leandro <lucio.leo85@gmail.com>
2020-12-01 20:09:13 -0300

    Rejecting import duplication

--- client/js/controllers/NegociacaoController.js
+++ client/js/controllers/NegociacaoController.js
@@ -61,6 +61,10 @@ class NegociacaoController {
         let service = new NegociacaoService();
 
         service.obterNegociacoes()
+            .then(negociacoes => negociacoes.filter(negociacao =>
+                    !this._listaNegociacoes.negociacoes.some(
+                    negociacaoExistente => JSON.stringify(negociacao) == JSON.stringify(negociacaoExistente)))
+                 )
             .then(negociacoes => negociacoes.forEach(negociacao => {
                 this._listaNegociacoes.add(negociacao);
                 this._mensagem.texto = 'Negociações da semana importada com sucesso';
