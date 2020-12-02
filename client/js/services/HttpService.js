0442a8191f8dedb26ece33826cf785bf0dbb2fcd
Lúcio Leandro <lucio.leo85@gmail.com>
2020-12-01 22:43:19 -0300

    Changed HMLHttpRequest to fetch api

--- client/js/services/HttpService.js
+++ client/js/services/HttpService.js
@@ -1,21 +1,34 @@
 class HttpService {
 
     get(url) {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
+
+        return fetch(url)
+            .then(res => this._handleErrors(res))
+            .then(res => res.json());
+
+
+         //Requisição ajax na unha
+        // return new Promise((resolve, reject) => {
+        //     let xhr = new XMLHttpRequest();
             
-            xhr.open('GET', url);
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                    if (xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-                        console.log(xhr.responseText);
-                        reject(xhr.responseText);
-                    }
-                }
-            }
-            xhr.send();
-        })
+        //     xhr.open('GET', url);
+        //     xhr.onreadystatechange = () => {
+        //         if (xhr.readyState == 4 && xhr.status == 200) {
+        //             if (xhr.status == 200) {
+        //                 resolve(JSON.parse(xhr.responseText));
+        //             } else {
+        //                 console.log(xhr.responseText);
+        //                 reject(xhr.responseText);
+        //             }
+        //         }
+        //     }
+        //     xhr.send();
+        // })
+    }
+
+    _handleErrors(res) {
+        if(!res.ok) throw new Error(res.statusText);
+
+        return res;
     }
 }
